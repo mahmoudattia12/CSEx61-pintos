1a318744e90da958c3efd57d610fe5628a4b11d8
mohamed <mo.hassan2792@gmail.com>
2023-04-25 02:17:13 +0200

    fixed-point

---
+++ src/threads/fixed-point.h
@@ -0,0 +1,23 @@
+int convert_to_fixed_point(int n);
+int convert_to_int(int f);
+int Add_2_Fixed_Point(int x,int y);
+int Sub_2_Fixed_Point(int x,int y);
+int Add_int_with_FP(int x,int n);
+int Sub_int_from_FP(int x,int n);
+int Multiply_2_FP(int x,int y);
+int Multiply_FP_by_int(int x,int n);
+int Divide_FPx_by_FPy(int x,int y);
+int Divide_x_by_n(int x, int n); 
+/*int makeFP_thenAdd(int x, int y);
+int makeFirstFP_thenADD(int x, int y);
+int makeFP_thenMultiply(int x, int y);
+int makeFirstFP_thenMultiply(int x, int y);
+int makeFP_thenDivide(int x, int y);
+int makeFirstFP_thenDivide(int x, int y);
+int makeSecondFP_thenDivide(int x, int y);
+
+#ifndef PINTOSPROJECT_FIXED_POINT_H
+#define PINTOSPROJECT_FIXED_POINT_H
+
+#endif //PINTOSPROJECT_FIXED_POINT_H
+*/
